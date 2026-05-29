bc763b21be2e3d0e2d2403540ec6ccb7a65d95d7
saikumarvarahabatla <40891012+saikumarvarahabatla@users.noreply.github.com>
2026-05-29 20:09:38 +0530

    Update app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+day = 3
+
+match day:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case _:
+        print("Invalid day")
